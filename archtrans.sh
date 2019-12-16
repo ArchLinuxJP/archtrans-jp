bc398af49d135e994d9a1a28c6a8add3b6db2617
syui <syui@syui.cf>
2019-12-16 10:35:35 +0900

    update jp check

--- archtrans.sh
+++ archtrans.sh
@@ -1,15 +1,43 @@
 #!/bin/bash
 
 if [ ! -f ./xq ];then
-	curl -sL https://github.com/syui/xq/releases/download/0.1/linux_amd64_xq -o xq
-	chmod +x xq
+    case $OSTYPE in
+	linux*)
+		curl -L https://github.com/syui/xq/releases/download/0.1/linux_amd64_xq -o xq
+		chmod +x xq
+	;;
+    	darwin*)
+	    	curl -L https://github.com/syui/xq/releases/download/0.1/darwin_amd64_xq -o xq
+		chmod +x xq
+	;;
+     esac
+fi
+
+if [ -z "$1" ];then
+    echo \$1
+    exit
 fi
 
 url=$1
 date_now=`date +"%Y%m%d"`
 url_arch="https://www.archlinux.org/feeds/news/"
+url_archjp="https://www.archlinux.jp/feeds/news.xml"
+
 xml=index.xml
+xmljp=news.xml
+
 curl -sL $url_arch -o $xml
+curl -sLO $url_archjp
+
+link=`./xq $xml|jq -r ".[0].link"`
+link=${link%*/}
+linkjp=`./xq $xmljp|jq -r ".[0].link"`
+linkjp=${linkjp%*/}
+
+if [ "${link##*/}" = "${linkjp##*/}" ];then
+	echo slack post webhook
+	exit
+fi
 
 date_xml=$date_now
 if [ "$2" != "t" ];then
@@ -22,6 +50,7 @@ fi
 
 title=`./xq $xml|jq -r ".[0]|.title"`
 body=`./xq $xml|jq -r ".[0]|.description"|tr -d '\n'`
+
 echo $title, $body
 curl -L -d "{\"txt\":\"$title\"}" $url
 curl -L -d "{\"txt\":\"$body\"}" $url
--- readme.md
+++ readme.md
@@ -1,5 +1,7 @@
 archlinux.orgのニュース更新があれば自動翻訳して、slackに投稿する`github-actions`です。更新は、1日おきにチェックします。
 
+なお、archlinux.jpがニュースを既に更新している場合、jpのnews-urlをslackに投稿します。
+
 - google app script
 
 - github actions
@@ -26,3 +28,8 @@ https://help.github.com/ja/actions/automating-your-workflow-with-github-actions/
 
 現在、`github.com/urfave/cli`が壊れてるため、`github.com/syui/xq`を最新版でbuildできない問題が発生中。したがって、go getではなくcurlしてます。
 
+### test
+
+github-actionsのtestは、slackのチャンネルをsyuiなどの個人変更し、コメントアウトすれば可能です。
+
+localでのtestは、urlを取得後、`archtrans.sh`など参照してください。
